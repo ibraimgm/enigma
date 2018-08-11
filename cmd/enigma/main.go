df1ebbdb6816ef01ad0ff97ef41f2480ee8f8731
Rafael Ibraim <ibraim.gm@gmail.com>
2018-08-11 00:34:56 -0300

    Added extra dependencies for future use

--- cmd/enigma/main.go
+++ cmd/enigma/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"time"
+
+	termbox "github.com/nsf/termbox-go"
+	getopt "github.com/pborman/getopt/v2"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	helpFlag := getopt.BoolLong("help", 'h', "Show usage and exit")
+	getopt.Parse()
+
+	if *helpFlag {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
+	if err := termbox.Init(); err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	termbox.Clear(termbox.ColorWhite, termbox.ColorBlack)
+
+	for i, c := range "Hello world!" {
+		termbox.SetCell(i, 0, c, termbox.ColorWhite, termbox.ColorDefault)
+	}
+
+	termbox.Flush()
+	time.Sleep(1 * time.Second)
 }
